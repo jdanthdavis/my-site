e7728cd23648c8542f5620052beb743cd6e61e1a
jdanthdavis <87504521+jdanthdavis@users.noreply.github.com>
2024-11-13 00:11:14 -0500

    Revert base change

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,4 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), commonjs()],
-  base: 'https://jdanthdavis.github.io/my-site/',
 });
